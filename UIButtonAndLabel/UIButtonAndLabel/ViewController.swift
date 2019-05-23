6a4e70e22e66b9fafccbb704f485369d4c2888df
NikolayPavlov8 <nikolaspavlov8@gmail.com>
2019-05-23 15:22:45 +0300

    code2

--- UIButtonAndLabel/UIButtonAndLabel/ViewController.swift
+++ UIButtonAndLabel/UIButtonAndLabel/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Это свойство скрытия надписи LABEL
+        label.isHidden = true
+        label.font = label.font.withSize(35)
+        label.textColor = UIColor.red
+        
+        button.setTitle("Get result", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.backgroundColor = .green
+        
     }
 
 
+    @IBAction func pressedButton(_ sender: UIButton) {
+        
+        if label.isHidden {
+            label.isHidden = false
+            label.text = "Hello, World!"
+            
+            button.setTitle("Clear", for: UIControl.State.normal)
+            button.setTitleColor(.white, for: .normal)
+            button.backgroundColor = .red
+        } else {
+            label.isHidden = true
+            
+            button.setTitle("Get result", for: .normal)
+            button.setTitleColor(.blue, for: .normal)
+            button.backgroundColor = .green
+        }
+    }
 }
 
